fa2c101c156ceb1d42fa620dbdac2d40c1c33997
Enrique <fpemlktz@gmail.com>
2021-10-18 12:53:12 +0200

    ...

--- app/src/main/java/com/enrique/empty4/LoginActivity.java
+++ app/src/main/java/com/enrique/empty4/LoginActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author kiketazo
+ */
 public class LoginActivity extends AppCompatActivity {
 
     @Override
